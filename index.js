d72a73bbd86164c9a98d2837f2cae0432aa13bf9
Mazck <81963893+Mazck@users.noreply.github.com>
2021-11-02 20:39:06 +0700

    a

--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require('express')();
-const img = require('./tho.json');
-app.get("/", function(req, res) { 
-return res.json({url: img[Math.floor(Math.random() * img.length)]})
-});
-app.listen(80)
-console.log("Server is listening on port 80...")
