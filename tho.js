2d168dd03e80ec09617eba249415a9004bac52f3
Mazck <81963893+Mazck@users.noreply.github.com>
2021-12-10 19:07:37 +0700

    a

--- tho.js
+++ tho.js
@@ -1,8 +1,19 @@
 {
     "data":{
-    "1":"1aaaaaaaaaaaaaaaaaaaaaaa",
-    "2":"2aaaaaaaaaaaaaaaaaaaaaaa",
-    "3":"3aaaaaaaaaaaaaaaaaaaaaaa",
-    "4":"4aaaaaaaaaaaaaaaaaaaaaaa"
+    "1":"Yêu một người không khó, em muốn làm điều đó cùng anh không ?",
+    "2":"Anh đã đi nhiều con đường, nhưng vẫn chưa tìm được cái đích mà anh muốn. Em là người duy nhất khiến anh lạc phương hướng, có thể trở thành vạch cán đích của anh không?",
+    "3":"Trời thì lạnh mà lòng anh thì ấm",
+    "4":"Em có tin vào tình yêu từ cái nhìn đầu tiên không, hay để anh thử đi lại qua em lần nữa?",
+    "5":"Em giúp anh bật Google Map được không, vì anh bị lạc đường trong trái tym em rồi.",
+    "6":"Tim em còn chỗ không, anh muốn chuyển nhà mà chưa tìm thấy chỗ.",
+    "7":"Ngoài kia đám cưới linh đình, bao giờ mới đến lượt mình em ơi",
+    "8":"Em quen mẹ anh mà sao không cho anh biết? Sáng bà mới hỏi con dâu đâu rồi đấy!những câu nói thả thính nhẹ nhàng",
+    "9":"Em có một siêu năng lực đó. Đấy là “Siêu thích anh~”",
+    "10":"Ở đây cấm mang đồ gây cháy nổ, nhưng sao anh lại đốt cháy được trái tim em?",
+    "11":" Định tặng anh một món quà đáng yêu, nhưng mà chuyển phát nhanh không cho em chui vào trong hộp.",
+    "12":"Tình yêu như mắt với tai.\nNếu thiếu một đứa tương lai còn gì.",
+    "13":"Trên trời hàng vạn ngôi sao\nSao anh chiếu hạng là sao lừa tình\nThế gian có vạn bóng hình\nLàm sao anh phải chung tình với em",
+    "14":"Học không lo học ở đây thơ ca cái lồn",
+    "15": "thính cái lồn gì lo học đi"
 	}
 }
